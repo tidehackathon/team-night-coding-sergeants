6d8e8d76ba24e672e1773c6c975bbd0fea816620
SIKER-98 <hubert.sikorski@yahoo.com>
2023-02-22 22:46:38 +0100

    Backend API skeleton

--- backend/analyse/controller.py
+++ backend/analyse/controller.py
@@ -1,21 +1,38 @@
 import operator
 from fastapi import APIRouter, HTTPException, status
-from .model import AnalyseModel
+from .model import NewsModel, ReturnNewsModel, ReturnTwitterStatsModel
 
 router = APIRouter(
     prefix='/analyse',
     tags=['analyse']
 )
 
+
 @router.get('/')
 async def analyse_get():
-
     return {
         'message': 'its working'
     }
 
-async def analyse_post(news:AnalyseModel):
+
+@router.post('/news')
+async def analyse_post(news: NewsModel):
+    # TODO: Analiza przez wszystkie modele
+
+    result = ReturnNewsModel()
 
     return {
-        'message':'received analyse model'
+        'prediction': result
     }
+
+
+@router.post('/twitter')
+async def analyse_twitter(file):
+    # TODO: Analiza pliku twitterowego
+
+    result = ReturnTwitterStatsModel()
+
+    return{
+        'prediction': result
+    }
+
--- backend/analyse/model.py
+++ backend/analyse/model.py
@@ -1,8 +1,19 @@
-from typing import Optional
 from pydantic import BaseModel
+from typing import List
 
 
-class AnalyseModel(BaseModel):
-    date: str
-    header: str
-    password: str
+class NewsModel(BaseModel):
+    text: str
+
+
+class ReturnNewsModel(BaseModel):
+    prediction: List[int] = []
+
+
+class ReturnTwitterStatsModel(BaseModel):
+    verifiedUsers: int = 100
+    verifiedTrue: int = 1000
+    verifiedFalse: int = 3000
+    unverifiedUsers: int = 400
+    unverifiedTrue: int = 2000
+    unverifiedFalse: int = 6000
